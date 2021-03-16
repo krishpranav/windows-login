28c7c671fbb254e31eefee51788aec1d8908e4fd
krishpranav <krisna.pranav@gmail.com>
2021-03-16 16:39:33 +0530

    done changes

--- windowslogin/Program.cs
+++ windowslogin/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Windows.Forms;
+using System.DirectoryServices.AccountManagement;
+using System.Drawing;
+using System.IO;
+using Microsoft.Win32;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace windowslogin
 {
-    class MainClass
+    static class Program
     {
-        public static void Main(string[] args)
+        ///<summary>
+        ///the main entry point for the application
+        ///</summary>
+        [STAThread]
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Initialize new screen
+            LogonScreen s = new LogonScreen();
+
+            // Set username
+            string SIZE = string.Empty;
+
+            try{
+                
+                UserPrincipal user = UserPrincipal.Current;
+                s.Username = user.SamAccountName;
+                s.DisplayName = user.DisplayName;
+                SID = user.Sid.Value;
+                s.Context = user.ContextType;
+            }
+            catch (Exception)
+            {
+                s.Username = Environment.UserName;
+                s.Context = ContextType.Machine;
+            }
         }
     }
 }
