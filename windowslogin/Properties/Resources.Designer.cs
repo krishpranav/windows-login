137d4b3705917ab2579ee06714aeffbe495a6d23
krishpranav <krisna.pranav@gmail.com>
2021-03-16 16:54:14 +0530

    done changes

---
+++ windowslogin/Properties/Resources.Designer.cs
@@ -0,0 +1,15 @@
+namespace FakeLogonScreen.Properties {
+    using System;
+
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResouceBuilder", "16.0.0.0")]
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    internal class Resources {
+
+        private static global::System.Resources.ResourceManager resourceMain;
+        
+        private static global::System.Globalization.CulutreInfo resourceCulture;
+
+        
+    }
+}
